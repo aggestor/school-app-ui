00ba8b06a4448a135d2fa66e7a953bd05c304ad5
aggestor <aggeemhl@gmail.com>
2023-08-13 10:28:03 +0200

    add: create-school ready to use for design

---
+++ src/components/Textarea.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="w-full focus-within:text-blue-600 text-gray-800 my-1.5 md:my-1">
+      <label v-if="!hideLabel" class="focus-within:font-semibold text-sm " :for="name">{{label}}</label>
+      <div :class="`${err ? 'border-red-500':''} border focus-within:text-blue-600 mt-1 focus-within:border-2 focus-within:border-blue-600 transition-all duration-300 h-28 px-2 rounded`">
+            <textarea onchange="onChange" :name="name" class="w-full h-full outline-none resize-none" :placeholder="placeholder"></textarea>
+      </div>
+      <span class="text-red-500 mt-0.5 text-[11px]" v-if="err">{{err}}</span>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    props: {
+          hideLabel:false,
+          name: String,
+          placeholder: String,
+          model: String,
+          value: String,
+          evt: String,
+          onChange: Function,
+          label: String,
+          err: String,
+      },
+  
+  }
+  </script>
--- src/pages/config/create-school.vue
+++ src/pages/config/create-school.vue
@@ -8,26 +8,45 @@
         </div>
         <div class="md:w-6/12 w-full h-[50%] flex items-center flex-col justify-center md:h-full">
             <span class="text-gray-800 text-center md:w-11/12 w-10/12">La création de votre compte a été éffectuée avec succès. Vous pouvez maintenant profitez de services de muyisphère en tant qu'utilisateur simple.</span>
-            <!-- <span @click="continueToLogin" class="bg-green-500 shadow-green-300 shadow-lg px-2 py-1.5 mt-5 hover:bg-green-600 rounded text-white cursor-pointer text-lg">Continuer</span> -->
         </div>
     </div>
     <div v-else data-aos="slide-up" data-aos-duration="500" class="md:w-10/12 w-full flex justify-between items-center h-[90%] rounded-lg border">
         <div class="md:w-7/12 flex h-[85%] md:p-6 p-4  justify-center flex-col" >
-            <h1 class="font-semibold text-2xl text-blue-600 mb-1">Créer un compte.</h1>
-            <p class="text-gray-700 text-sm">Saisissez les informations demadées ci-bas pour créer un compte de vendeur ou d'acheteur sur notre plateforme. </p>
+            <h1 class="font-semibold text-2xl text-blue-600 mb-1">School App.</h1>
+            <h2 class="font-semibold text-lg">Configuration école.</h2>
+            <p class="text-gray-700 text-sm">Remplissez le formulaire ci-bas pour configure l'école. </p>
             <form class="h-full w-full flex flex-col">
-                
+                <input  type="file" accept="image/png,image/jpeg,image/jpeg" hidden :ref="input" @change="handlePickImage" />
+                <TextBox  :onChange="handleInput" type="text" name="name" label="Nom" :value="name"  placeholder="Nom de l'école" :err="errors.name"/>
+                <div class="w-full flex space-x-3 justify-between">
+                    <TextBox  :onChange="handleInput" type="text" name="matrictule" label="Matricule" :value="matricule"  placeholder="Matricule de l'école" :err="errors.matricule"/>
+                    <TextBox  :onChange="handleInput" type="year" name="year" label="Annee de creation" :value="email" placeholder="Annee de creation" :err="errors.year"/>
+                    <TextBox  :onChange="handleInput" type="text" name="sigle" label="Sigle" :value="sigle" placeholder="Sigle" :err="errors.sigle"/>
+                </div>
+                <div class="w-full flex space-x-3 justify-between">
+                    <TextBox  :onChange="handleInput" type="email" name="email" label="Email" :value="email" placeholder="Email de l'ecole" :err="errors.email"/>
+                    <TextBox  :onChange="handleInput" type="tel" name="tel" label="Telephone" :value="tel" placeholder="Numero de telephone" :err="errors.tel"/>
+                </div>
+                <Textarea :onChange="handleInput" name="description" label="Description" placeholder="Parler un peu de votre ecole" :err="errors.description" :value="description"/>
             </form>
-            <p class="mb-2 text-sm">En créant un compte, vous acceptez <router-link to="/term-of-use"><span class="text-blue-600">les conditions générales de vente</span></router-link>  de muyiSphere.</p>
             <div class="w-full items-center my-3 flex  justify-between">
-                <ButtonLink text="Se connecter" path="/login" variant="default"/>
                 <BlueButtons @press="onPressRegister">
                     Enregistrer <CheckCircleIcon class="w-5 h-5 ml-1"/>
                 </BlueButtons>
             </div>
         </div>
         <div class="w-5/12 md:flex hidden items-center justify-center h-full">
-            <img src="/images/create-school.jpg" alt="Register image illustration" class="h-[430px] w-[430px] object-cover">
+          <div class="w-56 h-56 relative rounded border mx-auto">
+            <div class="w-full p-0.5 h-full">
+                <img :src="image" alt="Placeholder" class="w-full rounded h-full object-cover"/>
+            </div>
+            <span class="absolute -bottom-3 left-24">
+                <BlueButtons type="button" @press="onPressChooseImage">
+                    <PhotoIcon class="w-5 h-5"/>
+                    <PlusIcon class="w-5 h-5"/>
+                </BlueButtons>
+            </span>
+          </div>
         </div>
     </div>
 </div>
@@ -36,9 +55,20 @@
 
 <script setup>
 import { ref } from 'vue';
-import ButtonLink from '../../components/ButtonLink.vue';
+import {CheckCircleIcon, PhotoIcon} from "@heroicons/vue/24/outline"
 import BlueButtons from '../../components/v2/BlueButtons.vue';
-
+import TextBox from "../../components/TextBox.vue"
+import Textarea from '../../components/Textarea.vue';
+import { PlusIcon } from '@heroicons/vue/24/solid';
+const input = ref("input")
 const showSuccessPanel = ref(false)
+const file = ref()
+const image = ref('/placeholder-image.png')
+const errors = ref({})
 const handleChange = (e) =>{}
+const handlePickImage = (e) =>{
+    image.value = URL.createObjectURL(e.target.files[0])
+    file.value = e.target.files[0]
+}
+const onPressChooseImage = () => input.value.click()
 </script>
