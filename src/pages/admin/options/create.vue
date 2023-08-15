9e35ddc2699e16b0deb324b4143042e8988b053e
aggestor <aggeemhl@gmail.com>
2023-08-15 10:33:47 +0200

    add:option ready to use

--- src/pages/admin/options/create.vue
+++ src/pages/admin/options/create.vue
@@ -1,19 +1,18 @@
 <template>
-    <div class="w-11/12 mx-auto flex justify-center items-center md:h-[350px]">
+    <div class="w-11/12 mx-auto flex justify-center items-center md:h-[300px]">
         <div v-if="success" data-aos="slide-up" data-aos-duration="500" class="w-fit overflow-hidden flex  flex-col justify-between items-center h-auto ">
             <div class="w-full mb-3">
                 <h1 class="font-semibold text-2xl text-blue-600 mb-1">SchoolApp.</h1>
-                <h2 class="font-semibold text-lg">Création du cours.</h2>
+                <h2 class="font-semibold text-lg">Création d'une option.</h2>
             </div>
-            <SuccessComponent title="Création effectuée" message="La création du cours a été effectuée avec success. Vous pouvez maintenant assossier ce cours a tout ce qui peût y être lié." next="/ui/admin/courses" nextText="Suivant"/>
+            <SuccessComponent title="Création effectuée" message="La création de cette optino a été effectuée avec success. Vous pouvez maintenant assossier cette option a tout ce qui peût y être lié." next="/ui/admin/options" nextText="Suivant"/>
         </div>
         <div v-else data-aos="slide-up" data-aos-duration="500" class="md:w-6/12 w-full flex justify-between items-center h-[90%] rounded-lg border">
             <div class="w-full flex h-[85%] md:p-6 p-4  justify-center flex-col" >
-                <h2 class="font-semibold text-lg">Création cours.</h2>
-                <p class="text-gray-700 text-sm">Remplissez le formulaire ci-bas pour créer un nouveau cours. </p>
+                <h2 class="font-semibold text-lg">Création d'une option.</h2>
+                <p class="text-gray-700 text-sm">Remplissez le formulaire ci-bas pour créer  d'une option. </p>
                 <form class="h-full w-full flex flex-col">
-                    <TextBox  :onChange="handleInput" type="text" name="cours" label="Cours" :value="values.cours"  placeholder="Nom du cours" :err="errors.cours"/>
-                    <TextBox  :onChange="handleInput" type="text" name="total" label="Total" :value="values.total"  placeholder="Total du cours" :err="errors.total"/>
+                    <TextBox  :onChange="handleInput" type="text" name="option" label="Option" :value="values.option"  placeholder="Nom de l'option" :err="errors.option"/>
                 </form>
                 <div class="w-full items-center my-3 flex  justify-between">
                     <BlueButtons type="button" @press="onPressRegister">
@@ -33,7 +32,7 @@
     import BlueButtons from '../../../components/v2/BlueButtons.vue';
     import TextBox from "../../../components/TextBox.vue"
     import SuccessComponent from '../../../components/v2/SuccessComponent.vue';
-    import Course from '../../../api/v2/Course';
+    import Option from '../../../api/v2/Option';
     const success = ref(false)
     const errors = ref([])
     const values = ref({})
@@ -41,7 +40,7 @@
         values.value[e.target.name] = e.target.value
     }
     const onPressRegister = async () =>{
-        const result = await Course.create(values.value)
+        const result = await Option.create(values.value)
         if(result.error){
         errors.value = result.errorList 
         }
--- src/pages/admin/options/list.vue
+++ src/pages/admin/options/list.vue
@@ -3,33 +3,33 @@
         <div class="flex border-b mb-3 py-2 items-center justify-between">
             <div class="flex  items-center">
                 <GoBackAdminButton/>
-                <h1 class="text-lg font-semibold ml-2">Liste de tous les cours</h1>
+                <h1 class="text-lg font-semibold ml-2">Liste de toutes options</h1>
             </div>
            <div class="flex items-center">
-            <span class="w-7 h-7 mr-2 rounded bg-gray-200 grid place-items-center">{{ courses?.length}}</span>
-            <BlueLinkAsButton to="/ui/admin/courses/create">
-                <PlusIcon class="w-5 h-5 mr-1"/> Créer un cours
+            <span class="w-7 h-7 mr-2 rounded bg-gray-200 grid place-items-center">{{ options?.length}}</span>
+            <BlueLinkAsButton to="/ui/admin/options/create">
+                <PlusIcon class="w-5 h-5 mr-1"/> Créer une option
             </BlueLinkAsButton>
            </div>
         </div>
         <div class="border-b pb-1  font-semibold flex items-center justify-between">
             <span class="w-1/12">#</span>
-            <span class="w-3/12 flex justify-center">Cours</span>
-            <span class="w-3/12 flex justify-center">Cotation</span>
+            <span class="w-3/12 flex justify-center">options</span>
+            <span class="w-3/12 flex justify-center"></span>
             <span class="w-3/12 ">Maj</span>
             <span class="w-[10%] flex justify-center text-center">Actions</span>
         </div>
 
-        <div v-for="c of courses" :class="` py-2 text-sm  flex items-center justify-between ${courses.indexOf(c) % 2 != 0 ? 'bg-gray-100' :''}`">
-            <span class="w-1/12">{{courses.indexOf(c)+1 }}</span>
-            <span class="w-3/12 flex justify-center">{{c.cours }}</span>
-            <span class="w-3/12 flex justify-center">{{c.total }}</span>
-            <span class="w-3/12 flex ">{{formatDateToAgo(c.updated_at) }}</span>
+        <div v-for="o of options" :class="` py-2 text-sm  flex items-center justify-between ${options.indexOf(o) % 2 != 0 ? 'bg-gray-100' :''}`">
+            <span class="w-1/12">{{options.indexOf(o)+1 }}</span>
+            <span class="w-3/12 flex justify-center">{{o.option }}</span>
+            <span class="w-3/12 flex justify-center"></span>
+            <span class="w-3/12 flex ">{{formatDateToAgo(o.updated_at) }}</span>
             <span class="w-[10%] flex items-center justify-around">
-                <BlackLinkAsButton :to="'/ui/admin/courses/'+c.id+'/update'">
+                <BlackLinkAsButton :to="'/ui/admin/options/'+o.id+'/update'">
                     <PencilIcon class="w-5 h-5"/>
                 </BlackLinkAsButton>
-                <RedButtons @press="deleteCourse">
+                <RedButtons @press="deleteOption">
                     <TrashIcon class="w-5 h-5"/>
                 </RedButtons>
                 <!-- <BlueLinkAsButton :to="'/ui/admin/levels/'+l.id">
@@ -41,22 +41,21 @@
 </template>
 <script setup>
 import {onMounted, ref} from "vue"
-import Course from "../../../api/v2/Course";
+import Option from "../../../api/v2/Option";
 import { formatDateToAgo } from "../../../helpers/format-date";
 import BlueLinkAsButton from "../../../components/v2/BlueLinkAsButton.vue";
 import BlackLinkAsButton from "../../../components/v2/BlackLinkAsButton.vue";
 import RedButtons from "../../../components/v2/RedButtons.vue";
 import {  ArrowRightIcon, PencilIcon, PlusIcon, TrashIcon } from "@heroicons/vue/24/outline";
 import GoBackAdminButton from "../../../components/GoBackAdminButton.vue";
-const courses = ref([])
-const getCourses = async () =>{
-    const result = await Course.get()
+const options = ref([])
+const getOptions = async () =>{
+    const result = await Option.get()
     if(result.data){
-        console.log(result.data);
-        courses.value = result.data
+        options.value = result.data
     }
 }
 onMounted(()=>{
-    getCourses()
+    getOptions()
 })
 </script>
