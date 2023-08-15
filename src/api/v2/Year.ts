df2ff9d85c6415bb5523a69cd484aab2bf6af3e2
aggestor <aggeemhl@gmail.com>
2023-08-15 17:12:43 +0200

    add:update

--- src/api/v2/Year.ts
+++ src/api/v2/Year.ts
@@ -5,6 +5,9 @@ const Year = {
     async create(data: Record<string, any>){
         return await api.post('/year/create',data)
     },
+    async update(data: Record<string, any>, id){
+        return await api.put('/year/edit/'+id,data)
+    },
     async get(id?:string){
         return await api.get(id?'/year/'+id : '/year')
     },
