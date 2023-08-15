b5e53ca341acfbca807a0578ab30a51673379112
aggestor <aggeemhl@gmail.com>
2023-08-15 16:11:29 +0200

    add:update course start

--- src/api/v2/Course.ts
+++ src/api/v2/Course.ts
@@ -5,6 +5,9 @@ const Course = {
     async create(data: Record<string, any>){
         return await api.post('/cours/create',data)
     },
+    async update(data: Record<string, any>,id:string){
+        return await api.post('/cours/edit/'+id,data)
+    },
     async get(id?:string){
         return await api.get(id?'/cours/'+id : '/cours')
     },
