2f53e92a9c3e0e497e8f91ce263268bd10d61693
aggestor <aggeemhl@gmail.com>
2023-08-16 19:03:14 +0200

    add:list-users ready

--- src/pages/admin/users/list.vue
+++ src/pages/admin/users/list.vue
@@ -6,9 +6,9 @@
                 <h1 class="text-lg font-semibold ml-2">Liste des utilisateurs</h1>
             </div>
            <div class="flex items-center">
-            <span class="w-7 h-7 mr-2 rounded bg-gray-200 grid place-items-center">{{ courses?.length}}</span>
+            <span class="w-7 h-7 mr-2 rounded bg-gray-200 grid place-items-center">{{ users?.length}}</span>
             <BlueLinkAsButton to="/ui/admin/users/create">
-                <PlusIcon class="w-5 h-5 mr-1"/> Créer un utilisateur
+                <PlusIcon class="w-5 h-5 mr-1"/> Créer
             </BlueLinkAsButton>
            </div>
         </div>
@@ -17,50 +17,47 @@
             <span class="w-3/12 flex justify-center">Nom</span>
             <span class="w-3/12 flex justify-center">Email</span>
             <span class="w-2/12 flex justify-center">Phone</span>
-            <span class="w-1/12 flex justify-center">Role</span>
             <span class="w-1/12 ">Maj</span>
             <span class="w-[10%] flex justify-center text-center">Actions</span>
         </div>
 
-        <div v-for="c of courses" :class="` py-2 text-sm  flex items-center justify-between ${courses.indexOf(c) % 2 != 0 ? 'bg-gray-100' :''}`">
-            <span class="w-1/12">{{courses.indexOf(c)+1 }}</span>
-            <span class="w-3/12 flex justify-center">{{c.cours }}</span>
-            <span class="w-3/12 flex justify-center">{{c.total }}</span>
-            <span class="w-2/12 flex ">{{formatDateToAgo(c.updated_at) }}</span>
-            <span class="w-1/12 flex ">{{formatDateToAgo(c.updated_at) }}</span>
-            <span class="w-1/12 flex ">{{formatDateToAgo(c.updated_at) }}</span>
+        <div v-for="u of users" :class="` py-2 text-sm  flex items-center justify-between ${users.indexOf(u) % 2 != 0 ? 'bg-gray-100' :''}`">
+            <span class="w-1/12">{{users.indexOf(u)+1 }}</span>
+            <span class="w-3/12 flex justify-center">{{u.name }}</span>
+            <span class="w-3/12 flex justify-center">{{u.email }}</span>
+            <span class="w-2/12 flex ">{{u.tel}}</span>
+            <span class="w-1/12 flex ">{{formatDateToAgo(u.updated_at) }}</span>
             <span class="w-[10%] flex items-center justify-around">
-                <BlackLinkAsButton :to="'/ui/admin/courses/'+c.id+'/update'">
+                <BlackLinkAsButton :to="'/ui/admin/users/'+u.id+'/update'">
                     <PencilIcon class="w-5 h-5"/>
                 </BlackLinkAsButton>
                 <RedButtons @press="deleteCourse">
                     <TrashIcon class="w-5 h-5"/>
                 </RedButtons>
-                <!-- <BlueLinkAsButton :to="'/ui/admin/levels/'+l.id">
+                <BlueLinkAsButton :to="'/ui/admin/users/'+u.id">
                     <ArrowRightIcon class="w-5 h-5"/>
-                </BlueLinkAsButton> -->
+                </BlueLinkAsButton>
             </span>
         </div>
     </div>
 </template>
 <script setup>
 import {onMounted, ref} from "vue"
-import Course from "../../../api/v2/Course";
 import { formatDateToAgo } from "../../../helpers/format-date";
 import BlueLinkAsButton from "../../../components/v2/BlueLinkAsButton.vue";
 import BlackLinkAsButton from "../../../components/v2/BlackLinkAsButton.vue";
 import RedButtons from "../../../components/v2/RedButtons.vue";
 import {  ArrowRightIcon, PencilIcon, PlusIcon, TrashIcon } from "@heroicons/vue/24/outline";
 import GoBackAdminButton from "../../../components/GoBackAdminButton.vue";
-const courses = ref([])
-const getCourses = async () =>{
-    const result = await Course.get()
+import User from "../../../api/v2/User";
+const users = ref([])
+const getUsers = async () =>{
+    const result = await User.get()
     if(result.data){
-        console.log(result.data);
-        courses.value = result.data
+        users.value = result.data
     }
 }
 onMounted(()=>{
-    getCourses()
+    getUsers()
 })
 </script>
