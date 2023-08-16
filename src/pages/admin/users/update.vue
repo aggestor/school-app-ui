ab817fa8487449151abe4088e4622ce56354ca76
aggestor <aggeemhl@gmail.com>
2023-08-16 20:22:29 +0200

    add:update user not ready

--- src/pages/admin/users/update.vue
+++ src/pages/admin/users/update.vue
@@ -1,30 +1,30 @@
 <template>
-    <div class="w-11/12 mx-auto flex justify-center items-center md:h-[600px]">
+    <div class="w-11/12 md:w-7/12 mx-auto flex justify-center items-center md:h-[500px]">
         <div v-if="success" data-aos="slide-up" data-aos-duration="500" class="w-fit overflow-hidden flex  flex-col justify-between items-center h-auto ">
             <div class="w-full mb-3">
                 <h1 class="font-semibold text-2xl text-blue-600 mb-1">School App.</h1>
-                <h2 class="font-semibold text-lg">Configuration admininistrateur.</h2>
+                <h2 class="font-semibold text-lg">Création utilisateur.</h2>
             </div>
-            <SuccessComponent title="Configuration effectuée" message="La configuration de l'admininistrateur a été effectuée avec success. Passer a la configuration suivante en cliquant sur le bouton ci-bas." next="/login" nextText="Suivant"/>
+            <SuccessComponent title="Création effectuée" message="La création de l'utilisateur a été effectuée avec success. Il peut maintenant faire ce qui lui est attribué." next="/ui/admin/users" nextText="Suivant"/>
         </div>
-        <div v-else data-aos="slide-up" data-aos-duration="500" class="md:w-10/12 w-full flex justify-between items-center h-[90%] rounded-lg border">
-            <div class="md:w-7/12 flex h-[85%] md:p-6 p-4  justify-center flex-col" >
-                <div class="w-fit -ml-2 h-14">
-                <img src="/logo-with-ecrit.png" class="w-full h-full"/>
-            </div>
-                <h2 class="font-semibold text-lg">Configuration admininistrateur.</h2>
-                <p class="text-gray-700 text-sm">Remplissez le formulaire ci-bas pour configure l'admininistrateur. </p>
+        <div v-else data-aos="slide-up" data-aos-duration="500" class=" w-full flex justify-between items-center h-[90%] rounded-lg border">
+            <div class="w-full flex h-[85%] md:p-6 p-4  justify-center flex-col" >
+                <h2 class="font-semibold text-lg">Créer un utilisateur.</h2>
+                <p class="text-gray-700 text-sm">Remplissez le formulaire ci-bas pour créer un nouvel utilisateur. </p>
                 <form class="h-full w-full flex flex-col">
                     <div class="w-full flex space-x-3 justify-between">
-                        <TextBox  :onChange="handleInput" type="text" name="name" label="Nom" :value="values.name"  placeholder="Nom de l'école" :err="errors.name"/>
-                        <TextBox  :onChange="handleInput" type="email" name="email" label="Email" :value="values.email" placeholder="Email de l'ecole" :err="errors.email"/>
+                        <TextBox  :onChange="handleInput" type="text" name="name" label="Nom" :value="values.name"  placeholder="Nom de l'utilisateur" :err="errors.name"/>
+                        <TextBox  :onChange="handleInput" type="email" name="email" label="Email" :value="values.email" placeholder="Email de l'utilisateur" :err="errors.email"/>
                     </div>
                     <div class="w-full flex space-x-3 justify-between">
                         <TextBox  :onChange="handleInput" type="tel" name="tel" label="Telephone" :value="values.tel" placeholder="Numero de telephone" :err="errors.tel"/>
                         <TextBox  :onChange="handleInput" type="password" name="password" label="Mot de passe" :value="values.password" placeholder="Mot de passe" :err="errors.password"/>
                     </div>
-                    <div class="border w-full p-2 flex flex-wrap space-x-2 mt-2 h-40 rounded">
-                        <span v-for="role of roles" @click="addRole(role.id)" :class="`${chosenRoles.includes(role.id) ? 'text-white bg-blue-600' : 'bg-gray-200'} cursor-pointer p-0.5 px-1 space-x-3 text-sm w-fit h-fit rounded flex items-center`"  :key="role.id">{{role.role }} <CheckCircleIcon v-if="chosenRoles.includes(role.id)"  class='w-4 cursor-pointer h-4'/></span>
+                    <div class="border w-full p-2 mt-2 h-40 rounded">
+                        <p class="text-sm text-gray-700 mb-1.5">Attribuer un ou plusieurs roles</p>
+                        <div class=" flex flex-wrap space-x-2 ">
+                        <span v-for="role of roles" @click="addRole(role.id)" :class="`${chosenRoles.includes(role.id) ? 'text-white bg-blue-600' : 'bg-gray-200'} cursor-pointer p-0.5 px-1 space-x-3 text-sm w-fit h-fit rounded flex items-center`"  :key="role.id">{{role.role }} <CheckIcon v-if="chosenRoles.includes(role.id)"  class='w-4 cursor-pointer h-4'/></span>
+                    </div>
                     </div>
                 </form>
                 <div class="w-full items-center my-3 flex  justify-between">
@@ -32,16 +32,6 @@
                         Enregistrer <CheckCircleIcon class="w-5 h-5 ml-1"/>
                     </BlueButtons>
                 </div>
-                <div>
-                    <small>&copy; {{new Date().getFullYear() }} SchoolApp,<b>Modernic, Inc</b>.</small>
-                </div>
-            </div>
-            <div class="w-5/12 md:flex hidden items-center justify-center h-full">
-              <div class="w-72 h-72  mx-auto">
-                <div class="w-full h-full">
-                    <img src="/admin.jpg" alt="Placeholder" class="w-full rounded h-full object-cover"/>
-                </div>
-              </div>
             </div>
         </div>
         
@@ -51,24 +41,24 @@
     
     <script setup>
     import { onMounted, ref } from 'vue';
-    import {CheckCircleIcon} from "@heroicons/vue/24/outline"
-    import BlueButtons from '../../components/v2/BlueButtons.vue';
-    import TextBox from "../../components/TextBox.vue"
-    import Config from "../../api/v2/Config"
-    import Role from "../../api/v2/Role"
-    import SuccessComponent from '../../components/v2/SuccessComponent.vue';
+    import {CheckIcon,CheckCircleIcon} from "@heroicons/vue/24/outline"
+    import BlueButtons from '../../../components/v2/BlueButtons.vue';
+    import TextBox from "../../../components/TextBox.vue"
+    import Role from "../../../api/v2/Role"
+    import SuccessComponent from '../../../components/v2/SuccessComponent.vue';
+    import { useRoute } from 'vue-router';
+import User from '../../../api/v2/User';
     const success = ref(false)
-    const file = ref()
     const errors = ref([])
     const roles = ref([])
     const chosenRoles = ref([])
     const values = ref({})
+    const route = useRoute()
     const handleInput = (e) =>{
         values.value[e.target.name] = e.target.value
     }
     const onPressRegister = async () =>{
-        const result = await Config.addSU({...values.value,role_id:chosenRoles.value
-        })
+        const result = await User.update({...values.value,role_id:chosenRoles.value},route.params.id)
         if(result.error){
         errors.value = result.errorList 
         }
@@ -81,6 +71,13 @@
         if(result.data)
             roles.value = result.data
     }
+    const getUser = async () =>{
+        const result = await User.get(route.params.id)
+        if(result.data){
+            values.value = result.data[0]
+        }
+            
+    }
     const addRole = (name) =>{
         if(!chosenRoles.value.includes(name))
             chosenRoles.value.push(name)
@@ -89,6 +86,7 @@
         }
     } 
      onMounted(()=>{
+        getUser()
         getRoles()
      })
     </script>
