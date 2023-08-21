cff694f28cb57cddacc67d15f42c1a32500d8f17
aggestor <aggeemhl@gmail.com>
2023-08-21 15:26:25 +0200

    add:inscription

--- src/pages/admin/inscriptions/create.vue
+++ src/pages/admin/inscriptions/create.vue
@@ -9,10 +9,29 @@
         </div>
         <div v-else data-aos="slide-up" data-aos-duration="500" class="md:w-6/12 w-full flex justify-between items-center h-[90%] rounded-lg border">
             <div class="w-full flex h-[85%] md:p-6 p-4  justify-center flex-col" >
-                <h2 class="font-semibold text-lg">Création d'une option.</h2>
-                <p class="text-gray-700 text-sm">Remplissez le formulaire ci-bas pour créer  d'une option. </p>
+                <h2 class="font-semibold text-lg">Inscrire un eleve.</h2>
+                <p class="text-gray-700 text-sm">Remplissez le formulaire ci-bas pour inscrire  un eleve. </p>
                 <form class="h-full w-full flex flex-col">
                     <TextBox  :onChange="handleInput" type="text" name="option" label="Option" :value="values.option"  placeholder="Nom de l'option" :err="errors.option"/>
+                    <div class="flex w-full mt-2 items-center  justify-around">
+                        <div class="bg-white w-full bg-opacity-40 backd rounded border ">
+                            <div :class="` w-full  items-center ${searchResults.length > 0 && 'border-b'} h-9  px-1 flex space-x-2`">
+                                <input autofocus @input="handleSearch" :value="term" placeholder="Entrez le mom de l'eleve a inscrire." type="search" class="w-full placeholder:text-gray-400 text-gray-800 outline-none border-none mx-2 h-[80%] bg-transparent"><span @click="handleClickSearch"  class="text-gray-400 hover:bg-gray-100 rounded cursor-pointer"><MagnifyingGlassIcon class="h-7 w-7"/></span>
+                            </div>
+                            <div v-if="!isSearchLoading && searchResults.length == 0 && term.length > 0 && showSuggestionBox " class="h-64 grid place-items-center w-full">
+                                {{boxTitle }}
+                            </div>
+                            <div v-if="!isSearchLoading && searchResults.length > 0" class="h-64 overflow-auto __scrollbar grid place-items-center w-full">
+                                <div @click="handleClickProduct(s)" :key="s.prod_id" v-for="s of searchResults" class="p-1 w-full cursor-pointer hover:bg-blue-100 flex flex-col">
+                                    <p class="w-full font-semibold">{{s.prod_name }}</p>
+                                    <small class="text-gray-600">{{s.cat_name }}</small>
+                                </div>
+                            </div>
+                            <div v-if="isSearchLoading" class="h-64 grid place-items-center w-full">
+                                Loading...
+                            </div>
+                        </div>
+                    </div>
                 </form>
                 <div class="w-full items-center my-3 flex  justify-between">
                     <BlueButtons type="button" @press="onPressRegister">
@@ -34,8 +53,13 @@
     import SuccessComponent from '../../../components/v2/SuccessComponent.vue';
     import Option from '../../../api/v2/Option';
     const success = ref(false)
+    const isSearchLoading = ref(false)
+    const showSuggestionBox = ref(false)
+    const searchResults = ref([])
     const errors = ref([])
     const values = ref({})
+    const term = ref("")
+    const boxTitle = ref("")
     const handleInput = (e) =>{
         values.value[e.target.name] = e.target.value
     }
@@ -47,6 +71,9 @@
         if(result.success){
             success.value = true
         }
+    }
+    const handleSearch = e =>{
+
     }
     </script>
     
--- src/pages/admin/inscriptions/list.vue
+++ src/pages/admin/inscriptions/list.vue
@@ -3,12 +3,12 @@
         <div class="flex border-b mb-3 py-2 items-center justify-between">
             <div class="flex  items-center">
                 <GoBackAdminButton/>
-                <h1 class="text-lg font-semibold ml-2">Liste de toutes options</h1>
+                <h1 class="text-lg font-semibold ml-2">Liste des inscriptions</h1>
             </div>
            <div class="flex items-center">
             <span class="w-7 h-7 mr-2 rounded bg-gray-200 grid place-items-center">{{ options?.length}}</span>
-            <BlueLinkAsButton to="/ui/admin/options/create">
-                <PlusIcon class="w-5 h-5 mr-1"/> Créer une option
+            <BlueLinkAsButton to="/ui/admin/inscriptions/create">
+                <PlusIcon class="w-5 h-5 mr-1"/> Inscrire
             </BlueLinkAsButton>
            </div>
         </div>
@@ -26,7 +26,7 @@
             <span class="w-3/12 flex justify-center"></span>
             <span class="w-3/12 flex ">{{formatDateToAgo(o.updated_at) }}</span>
             <span class="w-[10%] flex items-center justify-around">
-                <BlackLinkAsButton :to="'/ui/admin/options/'+o.id+'/update'">
+                <BlackLinkAsButton :to="'/ui/admin/inscriptions/'+o.id+'/update'">
                     <PencilIcon class="w-5 h-5"/>
                 </BlackLinkAsButton>
                 <RedButtons @press="deleteOption">
