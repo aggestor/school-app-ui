7e3c01e53f35b13040efbe03400be14ec0b7fb8a
aggestor <aggeemhl@gmail.com>
2023-08-21 18:02:08 +0200

    add:subscribe inscription

--- src/api/v2/Student.ts
+++ src/api/v2/Student.ts
@@ -11,6 +11,9 @@ const Student = {
     async get(id?:string){
         return await api.get(id?'/eleve/'+id : '/eleve')
     },
+    async search(term:string){
+        return await api.get('/eleve/identite/'+term )
+    },
 }
 
 export default Student
--- src/pages/admin/inscriptions/create.vue
+++ src/pages/admin/inscriptions/create.vue
@@ -1,30 +1,33 @@
 <template>
-    <div class="w-11/12 mx-auto flex justify-center items-center md:h-[300px]">
+    <div class="w-11/12 mx-auto flex justify-center items-center md:min-h-[300px]">
         <div v-if="success" data-aos="slide-up" data-aos-duration="500" class="w-fit overflow-hidden flex  flex-col justify-between items-center h-auto ">
             <div class="w-full mb-3">
                 <h1 class="font-semibold text-2xl text-blue-600 mb-1">SchoolApp.</h1>
-                <h2 class="font-semibold text-lg">Création d'une option.</h2>
+                <h2 class="font-semibold text-lg">Inscrire un eleve.</h2>
             </div>
             <SuccessComponent title="Création effectuée" message="La création de cette optino a été effectuée avec success. Vous pouvez maintenant assossier cette option a tout ce qui peût y être lié." next="/ui/admin/options" nextText="Suivant"/>
         </div>
-        <div v-else data-aos="slide-up" data-aos-duration="500" class="md:w-6/12 w-full flex justify-between items-center h-[90%] rounded-lg border">
+        <div v-else data-aos="zoom-in" data-aos-duration="500" class="md:w-6/12 w-full flex justify-between items-center h-[90%] rounded-lg border">
             <div class="w-full flex h-[85%] md:p-6 p-4  justify-center flex-col" >
                 <h2 class="font-semibold text-lg">Inscrire un eleve.</h2>
                 <p class="text-gray-700 text-sm">Remplissez le formulaire ci-bas pour inscrire  un eleve. </p>
                 <form class="h-full w-full flex flex-col">
-                    <TextBox  :onChange="handleInput" type="text" name="option" label="Option" :value="values.option"  placeholder="Nom de l'option" :err="errors.option"/>
+                    <!-- <TextBox  :onChange="handleInput" type="text" name="option" label="Option" :value="values.option"  placeholder="Nom de l'option" :err="errors.option"/> -->
+                    <Datalist :err="errors.classe_id" label="Option" :onChange="handleInput" name="classe_id" :value="values.classe_id"  placeholder="Choisir Option">
+                        <option  :key="o.id" v-for="o of classes">{{o.name }}</option>
+                    </Datalist>
                     <div class="flex w-full mt-2 items-center  justify-around">
                         <div class="bg-white w-full bg-opacity-40 backd rounded border ">
                             <div :class="` w-full  items-center ${searchResults.length > 0 && 'border-b'} h-9  px-1 flex space-x-2`">
-                                <input autofocus @input="handleSearch" :value="term" placeholder="Entrez le mom de l'eleve a inscrire." type="search" class="w-full placeholder:text-gray-400 text-gray-800 outline-none border-none mx-2 h-[80%] bg-transparent"><span @click="handleClickSearch"  class="text-gray-400 hover:bg-gray-100 rounded cursor-pointer"><MagnifyingGlassIcon class="h-7 w-7"/></span>
+                                <input name="student" autofocus @input="handleInput" :value="values.student" placeholder="Entrez le mom de l'eleve a inscrire." type="search" class="w-full placeholder:text-gray-400 text-gray-800 outline-none border-none mx-2 h-[80%] bg-transparent"><span @click="handleClickSearch"  class="text-gray-400 hover:bg-gray-100 rounded cursor-pointer"><MagnifyingGlassIcon class="h-7 w-7"/></span>
                             </div>
-                            <div v-if="!isSearchLoading && searchResults.length == 0 && term.length > 0 && showSuggestionBox " class="h-64 grid place-items-center w-full">
+                            <div v-if="!isSearchLoading && searchResults.length == 0 && values?.student?.length > 0 && showSuggestionBox " class="h-64 grid place-items-center w-full">
                                 {{boxTitle }}
                             </div>
                             <div v-if="!isSearchLoading && searchResults.length > 0" class="h-64 overflow-auto __scrollbar grid place-items-center w-full">
-                                <div @click="handleClickProduct(s)" :key="s.prod_id" v-for="s of searchResults" class="p-1 w-full cursor-pointer hover:bg-blue-100 flex flex-col">
-                                    <p class="w-full font-semibold">{{s.prod_name }}</p>
-                                    <small class="text-gray-600">{{s.cat_name }}</small>
+                                <div @click="handleClickProduct(s)" :key="s.id" v-for="s of searchResults" class="p-1 w-full cursor-pointer hover:bg-blue-100 flex flex-col">
+                                    <p class="w-full font-semibold">{{s.names+' '+s.firstname+' '+s.lastname }}</p>
+                                    <small class="text-gray-600">{{s.genre }} | {{s.tel }}</small>
                                 </div>
                             </div>
                             <div v-if="isSearchLoading" class="h-64 grid place-items-center w-full">
@@ -46,25 +49,49 @@
     </template>
     
     <script setup>
-    import { ref } from 'vue';
+    import { ref, onMounted } from 'vue';
     import {CheckCircleIcon} from "@heroicons/vue/24/outline"
     import BlueButtons from '../../../components/v2/BlueButtons.vue';
-    import TextBox from "../../../components/TextBox.vue"
+    import Datalist from "../../../components/Datalist.vue"
     import SuccessComponent from '../../../components/v2/SuccessComponent.vue';
-    import Option from '../../../api/v2/Option';
+    import Class from '../../../api/v2/Class';
+    import Student from '../../../api/v2/Student';
+    import Inscription from '../../../api/v2/Inscription';
+
     const success = ref(false)
     const isSearchLoading = ref(false)
     const showSuggestionBox = ref(false)
     const searchResults = ref([])
     const errors = ref([])
+    const classes = ref([])
+    const classId = ref('')
+    const studentId = ref('')
     const values = ref({})
-    const term = ref("")
     const boxTitle = ref("")
-    const handleInput = (e) =>{
+    const handleInput = async(e) =>{
         values.value[e.target.name] = e.target.value
+        if(e.target.name == "classe_id"){
+            const _classes = classes.value.filter(c =>c.name == e.target.value)
+            if(_classes[0]){
+                classId.value = _classes[0].id
+            }
+        }
+        if(e.target.name == 'student'){
+           await searchStudent(e)
+        }
+    }
+    const searchStudent  = async e =>{
+        const result = await Student.search(e.target.value)
+        if(result.data){
+            searchResults.value = result.data
+        }
+    }
+    const handleClickProduct  =  s =>{
+        values.value.student = s.names+' '+s.firstname+' '+s.lastname
+        studentId.value = s.id
     }
     const onPressRegister = async () =>{
-        const result = await Option.create(values.value)
+        const result = await Inscription.subscribe(classId.value, studentId.value)
         if(result.error){
         errors.value = result.errorList 
         }
@@ -72,8 +99,14 @@
             success.value = true
         }
     }
-    const handleSearch = e =>{
-
+    const getClasses =  async () =>{
+        const result = await Class.get()
+        if(result.data){
+            classes.value = result.data
+        }
     }
+    onMounted(()=>{
+        getClasses()
+    })
     </script>
     
