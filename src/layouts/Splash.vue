6b1feb3ac5c075a722ede200f92e63ee1ddd5e90
aggestor <aggeemhl@gmail.com>
2023-08-21 14:27:20 +0200

    add:added loading...

---
+++ src/layouts/Splash.vue
@@ -0,0 +1,11 @@
+<template>
+    <section class="w-full h-full bg-white ">
+        <main class="w-full grid place-items-center  h-full">
+            <h1 class="font-semibold text-lg">Loading...</h1>
+        </main>
+    </section>
+</template>
+<script>
+
+export default {}
+</script>
