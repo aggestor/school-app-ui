51ed87cab3016af549a68353ebbb96c738ce212c
aggestor <aggeemhl@gmail.com>
2023-08-21 15:03:16 +0200

    add:log in splash screen"

--- src/layouts/Splash.vue
+++ src/layouts/Splash.vue
@@ -1,7 +1,8 @@
 <template>
-    <section class="w-full h-full bg-white ">
+    <section class="w-full h-screen bg-white ">
         <main class="w-full grid place-items-center  h-full">
-            <h1 class="font-semibold text-lg">Loading...</h1>
+            <h1 class="font-semibold text-lg">
+                <img src="/logo-with-ecrit.png" class="md:w-[65%] w-[90%]  h-16"/></h1>
         </main>
     </section>
 </template>
