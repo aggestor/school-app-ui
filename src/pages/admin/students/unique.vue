18dd3c848590d2df996d47ab41eb9999549297e2
aggestor <aggeemhl@gmail.com>
2023-08-24 16:49:03 +0200

    add:unique

--- src/pages/admin/students/unique.vue
+++ src/pages/admin/students/unique.vue
@@ -1,13 +1,19 @@
 <template>
     <div class="grid grid-cols-12 p-2">
-        <div class="col-span-6 flex items-center justify-around h-48 border rounded">
+        <div class="col-span-5 flex items-center justify-around h-40 border rounded">
             <div>
-                <img src="/user-avatar.png" class="w-40 h-40 text-gray-500"/>
+                <img src="/user-avatar.png" class="w-32 h-32 text-gray-500"/>
             </div>
-            <div class="w-[calc(100%-170px)] p-2 border">
+            <div class="w-[calc(100%-170px)] p-2">
                 <h1 class="font-semibold text-xl">{{data.names+" "+data.firstname+" "+data.lastname }}</h1>
                 <div class="flex space-x-2 items-center">
                     <span class="bg-green-200 text-green-700 p-0.5 rounded">{{uppercaseFirst(data?.genre) }}</span>
+                    <a :href="`tel:${data?.tel}`" class="hover:underline text-blue-600">
+                        {{data?.tel}}
+                    </a>
+                </div>
+                <div class="my-1 text-gray-600">
+                    {{ data?.name }}({{data?.option }})
                 </div>
             </div>
         </div>
