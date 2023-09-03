c41536219905b2c4fd7dca165cc4899f2056f9f4
aggestor <aggeemhl@gmail.com>
2023-09-03 11:54:21 +0200

    add:update format date to ago fixed day not showing prefix

--- src/helpers/format-date.js
+++ src/helpers/format-date.js
@@ -2,7 +2,9 @@ import moment from "moment/moment"
 import  * as fr from "moment/locale/fr"
 
 export const formatDateToAgo = (input) =>{
-   return moment(input).local(fr).fromNow(true).substring(0,3).trim()
+    const date = moment(input).local(fr).fromNow(true)
+    if(date.includes("jours")) return date.substring(0,4).trim() ;
+   return date.substring(0,3).trim()
 }
 export const formatDateNormal = (input) =>{
     return moment(input).local(fr).format("ll")
