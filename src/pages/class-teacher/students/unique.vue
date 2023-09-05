630a86bf2d50b67e49cc26d03a1aac3e5e8f0da8
aggestor <aggeemhl@gmail.com>
2023-09-05 18:51:04 +0200

    add:uniq for teacher ready

--- src/pages/class-teacher/students/unique.vue
+++ src/pages/class-teacher/students/unique.vue
@@ -1,5 +1,6 @@
 <template>
-    <div class="w-full p-2">
+    <div class="w-full text-gray-800 p-2">
+        <h1 class="text-xl mt-2 mb-3 font-bold">Identification</h1>
         <div class="w-full flex items-center  h-auto p-2 border rounded">
             <div class="w-3/12">
                 <img src="/man.jpg" class="w-32 rounded object-cover  h-32 text-gray-500"/>
@@ -46,6 +47,38 @@
                     <p class="text-gray-600">{{data.name }}</p>
                 </div>
             </div>
+        </div>
+        <div>
+            <h1 class="text-xl mt-2 mb-3 font-bold">Cotes</h1>
+        </div>
+        <div class="w-full flex items-center  h-auto p-2 border rounded">
+            <div class="border-b pb-1 w-full  font-semibold flex items-center justify-between">
+            <span class="w-1/12">#</span>
+            <span class="w-2/12 flex justify-center">Cours</span>
+            <span class="w-2/12 flex justify-center">Periode</span>
+            <span class="w-2/12 flex justify-center">Cote</span>
+            <span class="w-2/12 flex justify-center">Total</span>
+            <span class="w-1/12 text-center ">Maj</span>
+            <span class="w-[10%] flex justify-center text-center">Actions</span>
+        </div>
+
+        <!-- <div v-for="o of data?.data" :class="` py-2 text-sm  flex items-center justify-between ${data?.data?.indexOf(o) % 2 != 0 ? 'bg-gray-100' :''}`">
+            <span class="w-1/12">{{data?.data?.indexOf(o)+1 }}</span>
+            <span class="w-3/12 flex justify-center">{{o.names+' '+o.firstname+' '+o.lastname }}</span>
+            <span class="w-2/12 flex justify-center">{{o.matricule }}</span>
+            <span class="w-2/12 flex justify-center">{{o.cours }}</span>
+            <span class="w-2/12 flex justify-center">{{parseRatingType(o.cotes_types) }}</span>
+            <span class="w-1/12 flex justify-center"><span :class="o.cotes >= o.total_periode/2 ? 'bg-green-100 border border-green-300  text-green-600 rounded p-0.5': 'bg-red-100 border border-red-300 text-red-600 rounded p-0.5'">{{o.cotes }}</span></span>
+            <span class="w-1/12 flex justify-center ">{{formatDateToAgo(o.updated_at) }}</span>
+            <span class="w-[10%] flex items-center justify-around">
+                <BlackLinkAsButton :to="'/ui/class-teacher/rating/'+o.id+'/'+prettyString(o.names+' '+o.firstname+' '+o.lastname )+'/'+o.matricule+'/'+prettyString(o.cours)+'/update'">
+                    <PencilIcon class="w-5 h-5"/>
+                </BlackLinkAsButton>
+                <RedButtons @press="onClickDelete(o)">
+                    <TrashIcon class="w-5 h-5"/>
+                </RedButtons>
+            </span>
+        </div> -->
         </div>
     </div>
 </template>
